b15c4b2fc69d2b7e693d1f4b0c114e0d04f269b0
hdesk1038 <hdesk@igidr.ac.in>
2025-08-01 10:27:05 +0530

    Update ErrorPage.jsx

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -2,16 +2,27 @@ import { useRouteError } from "react-router";
 
 const ErrorPage = () => {
     const error = useRouteError();
-    console.log(error)
+    console.log(error);
+
+    const errorMessage = error
+        ? `${error.status + " : " || ""} ${error.statusText || error.message || "Unknown error"}`
+        : "";
+
     return (
-        <div className="min-h-screen flex items-center justify-center bg-red-50 text-center">
-            <div>
-                <img src="https://i.pinimg.com/1200x/13/75/7f/13757ff4b62025394ea27aaf46fe12ae.jpg" className="w-[300px]" alt="" />
+        <div className="text-center">
+            <div className="flex items-center flex-col justify-center min-h-screen ">
+                <img
+                    src="https://i.pinimg.com/1200x/13/75/7f/13757ff4b62025394ea27aaf46fe12ae.jpg"
+                    className="w-[300px] "
+                    alt="Error Illustration"
+                />
                 <h1 className="text-4xl font-bold text-red-700 mb-4">Oops! 😓</h1>
                 <p className="text-lg text-red-500">Something went wrong.</p>
-                <p className="mt-3 text-gray-600">
-                    <i>{error.status} : {error.statusText || error.message}</i>
-                </p>
+                {error && (
+                    <p className="mt-3 text-gray-600">
+                        <i>{errorMessage}</i>
+                    </p>
+                )}
             </div>
         </div>
     );

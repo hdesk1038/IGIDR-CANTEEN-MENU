2297f631310698d493fd1c9e283daad46ae1115b
hdesk1038 <hdesk@igidr.ac.in>
2025-08-01 16:11:19 +0530

    Update ErrorPage.jsx

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -11,6 +11,11 @@ const ErrorPage = () => {
     return (
         <div className="text-center">
             <div className="flex items-center flex-col justify-center min-h-screen ">
+                <img
+                    src="https://indianmemetemplates.com/wp-content/uploads/Bhai-kya-kar-raha-hai-tu.jpg"
+                    className="w-[300px] "
+                    alt="Error Illustration"
+                />
                 <h1 className="text-4xl font-bold text-red-700 mb-4">Oops! 😓</h1>
                 <p className="text-lg text-red-500">Something went wrong.</p>
                 {error && (
